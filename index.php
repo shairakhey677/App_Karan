1786356b55d40dc5c762ea32cf702209060ec48e
PakBass <andhibasuki@gmail.com>
2023-10-13 10:00:26 +0800

    Mengubah background menjadi hitam

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   </head>
-  <body>
+  <body style="background-color:black;">
     <div class="container">
         <div class="row d-flex align-items-center justify-content-center" style="height: 100vh; width:100%">
             <div class="col-md-6">
